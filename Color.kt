ae92a1a3742c295c735157c94d4196a7199db03b
cpappas-mobeetest <cpappas@mobeetest.com>
2025-12-07 14:07:41 +0200

    Add files via upload

---
+++ Color.kt
@@ -0,0 +1,46 @@
+package com.mobeetest.worker.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Permission Activity Colors
+
+// Scaffold
+
+// Top Bar
+val permissionsScaffoldTopBarBackground = Color(0xFFD3ABF7)
+val permissionsScaffoldTopBarText = Color(0xFFFFFFFF)
+val permissionsScaffoldTopBarBottomBorder = Color(0xFF1A6B55)
+val permissionsScaffoldTopBarBottomShadow = Color(0x661A84BB)
+val permissionsScaffoldTopBarIcon = Color(0xFFFFFFFF)
+//val permissionsScaffoldTopBarDropdownBackground = Color(0xFFD3ABF7)
+val DividerLineColor = Color(0x33000000)  // 20% black for separators
+
+
+// Content area
+
+val permissionsScaffoldContentCircularProgressIndicatorColor = Color(0xFFFFFFFF)
+
+val bootStrapAlertSuccess = Color(0xFFD9F8E1)
+val bootStrapAlertSuccessBorder = Color(0xFF1A933A)
+val bootStrapAlertSuccessSeperatorColor = Color(0xFF6B8E23)
+
+
+val bootStrapAlertSuccessShadowColor = Color(0xFFB2F2BB)
+val bootStrapAlertSuccessTextColor = Color(0x881E2A02)
+val bootStrapInfoSuccessTextColor = Color(0x881E2A02)
+
+
+// ===============================
+// MAIN ACTIVITY / SCAFFOLD COLORS
+// ===============================
+
+val mainScaffoldTopBarBackground = Color(0xFFD3ABF7)
+val mainScaffoldTopBarText = Color(0xFFFFFFFF)
+val mainScaffoldTopBarBottomBorder = Color(0xFF1A6B55)
+val mainScaffoldTopBarBottomShadow = Color(0x661A84BB)
+val mainScaffoldTopBarIcon = Color(0xFFFFFFFF)
+
+// Bottom navigation bar
+val mainBottomBarBorder = Color(0xFF1A6B55)
+val mainBottomBarBackground = Color(0xFFFFD700)
+val mainBottomBarSelectedItemBackground = Color(0xFFEEE680)
